3621ff8a60ea10423a0f03e1f7380bb5a9aa22bb
fpesce27 <fpesce@frba.utn.edu.ar>
2022-11-29 04:45:53 -0300

    primera version terminada

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import './App.css';
-import React from 'react';
-import { Route, Routes, Link } from "react-router-dom";
-import About from './routes/About';
-import Home from './routes/Home';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div>
-          <nav>
-            <ul id="navigation">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     );
